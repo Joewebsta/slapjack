0494bcb3b4d57eff6a684570b281ccb5fcbdcc21
Joe Webster <joseph.scott.webster@gmail.com>
2020-11-21 12:00:46 -0500

    Create Game class and create pseudocode for class functionality

---
+++ src/game.js
@@ -0,0 +1,22 @@
+class Game {
+
+  constructor() {
+    // 2 players
+    // Afray of all possible cards?
+    // Central pile of cards
+    // Player's whose current turn it is
+  }
+}
+
+
+// Create instance of game on load
+// Create array of all cards
+// Shuffle array of cards
+// Create two player instances deal half of cards to Player constructor as arguments
+// Player deals card to central pile
+// Player slaps card
+
+// SHUFFLE ARRAY
+// Use Math.random to select random index between zero and (length - 1)
+// Add index to new array and remove index from original array
+// Continue until orginal array length is zero.
