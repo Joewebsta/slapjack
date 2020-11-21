43b6f5982aab079e40bcb4b6dfe426f3d4786613
Joe Webster <joseph.scott.webster@gmail.com>
2020-11-21 11:36:55 -0500

    Create Player class

---
+++ src/player.js
@@ -0,0 +1,17 @@
+class Player {
+  
+  constructor(name, hand) {
+    this.name = name;
+    this.id = Date.now();
+    this.wins = 0;
+    this.hand = hand;
+  }
+
+  playCard() {
+
+  }
+
+  saveWinsToStorage() {
+
+  }
+}
