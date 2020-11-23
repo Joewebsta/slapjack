8cd5f9157c717402130797a8ce7b5f08f6fb986a
Joe Webster <joseph.scott.webster@gmail.com>
2020-11-23 18:04:42 -0500

    Click 'q' or 'p' to playCard and log to console

--- src/game.js
+++ src/game.js
@@ -5,8 +5,7 @@ class Game {
     this.player2 = player2;
     this.cards = cards;
     this.centralPile = [];
-    this.currentPlayerTurn = [this.player1];
-    console.log('Game created!');
+    this.currentPlayerTurn = this.player1;
   }
   
   shuffleCards(cards) {
@@ -22,19 +21,26 @@ class Game {
 
   dealPlayerDecks() {
     this.player1.hand = this.cards.slice(0, 26);
-    this.player2.hand = this.cards.slice(27, 52);
+    this.player2.hand = this.cards.slice(26, 52);
+  }
+
+  handlePlayerActions(e) {
+    if (e.key === 'q' || e.key === 'p') this.dealCard(e);
+  }
+
+  dealCard(e) {
+    if (e.key === 'q' && this.currentPlayerTurn === this.player1) {
+      this.centralPile.unshift(this.player1.playCard());
+      this.currentPlayerTurn = this.player2;
+      console.log(this.centralPile[0].suit);
+    }
+
+    if (e.key === 'p' && this.currentPlayerTurn === this.player2) {
+      this.centralPile.unshift(this.player2.playCard());
+      this.currentPlayerTurn = this.player1;
+      console.log(this.centralPile[0].suit);
+    }
   }
 }
 
-
-// Create instance of game on load
-// Create array of all cards
-// Shuffle array of cards
-// Create two player instances deal half of cards to Player constructor as arguments
-// Player deals card to central pile
 // Player slaps card
-
-// SHUFFLE ARRAY
-// Use Math.random to select random index between zero and (length - 1)
-// Add index to new array and remove index from original array
-// Continue until orginal array length is zero.
--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+const game = initializeGame();
+
+
 function initializeGame() {
   const cards = createCards();
   const player1 = new Player('Player 1');
@@ -19,5 +22,6 @@ function createCards() {
   }, []);
 }
 
-const game = initializeGame();
-console.log(game);
+window.addEventListener('keypress', function(e) {
+  game.handlePlayerActions(e);
+}.bind(game), false);
