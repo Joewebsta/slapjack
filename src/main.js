cd5dad41cbb46aba52ca8ad08d3145799f92f582
Joe Webster <joseph.scott.webster@gmail.com>
2020-11-27 14:41:02 -0500

    Update central card when players deal a card

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
+const centralCardImg = document.querySelector('.js-central-card-img');
+
 const game = Game.initializeGame();
 game.dealPlayerDecks();
 
 window.addEventListener('keypress', function(e) {
   game.handlePlayerActions(e);
 }.bind(game), false);
+
+function updateCentralCard(cardSrc) {
+  centralCardImg.src = cardSrc;
+}
