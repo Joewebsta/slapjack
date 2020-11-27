f948ccb3acba8e97bd89bb7952d02a119a06d5b0
Joe Webster <joseph.scott.webster@gmail.com>
2020-11-27 14:56:53 -0500

    Create card image if it does not exist

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const centralCardImg = document.querySelector('.js-central-card-img');
+const centralCard = document.querySelector('.js-central-card');
 
 const game = Game.initializeGame();
 game.dealPlayerDecks();
@@ -8,5 +8,17 @@ window.addEventListener('keypress', function(e) {
 }.bind(game), false);
 
 function updateCentralCard(cardSrc) {
+  const centralCardImg = document.querySelector('.js-central-card-img');
+
+  if (!centralCardImg) {
+    createCentralCardImg(cardSrc);
+    return;
+  }
+  
   centralCardImg.src = cardSrc;
+}
+
+function createCentralCardImg(src) {
+  const cardImgHTML = `<img class="js-central-card-img" src="${src}" alt="">`;
+  centralCard.insertAdjacentHTML('afterbegin', cardImgHTML);
 }
